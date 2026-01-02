c4aeb222dd2603cf136172e0e061c350ad93457e
HowlingCoder <howlingcoder@gmail.com>
2026-01-02 18:21:34 +0100

    Updated +layout.ts

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,19 +2,11 @@
 	import favicon from '$lib/assets/favicon.svg';
 	import Dock from '$lib/components/layout/dock.svelte';
 	import Header from '$lib/components/layout/header.svelte';
-	import { loadTranslations } from '$lib/translations/translations';
-	import { onMount } from 'svelte';
+	import { loading } from '$lib/translations/translations';
 
 	import '../app.css';
 
-    let { children, data } = $props();
-
-	onMount(async () => {
-	
-        const initLocale = 'en';
-        const pathname = data.pathname || '/';
-        await loadTranslations(initLocale, pathname);
-	});
+    let { children } = $props();
 </script>
 
 <svelte:head>
@@ -24,12 +16,18 @@
 	/>
 </svelte:head>
 
-<div class="flex flex-col h-screen">
-	<Header class="flex-none" />
+{#if $loading}
+	<div class="flex items-center justify-center h-screen">
+		<span class="loading loading-spinner loading-lg"></span>
+	</div>
+{:else}
+	<div class="flex flex-col h-screen">
+		<Header class="flex-none" />
 
-	<main class="flex-1 overflow-y-auto" style="max-height: calc(100vh - 138px);">
-		{@render children()}
-	</main>
+		<main class="flex-1 overflow-y-auto" style="max-height: calc(100vh - 138px);">
+			{@render children()}
+		</main>
 
-	<Dock class="flex-none" />
-</div>
+		<Dock class="flex-none" />
+	</div>
+{/if}
--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,7 +1,12 @@
 import type { LayoutLoad } from './$types';
+import { loadTranslations } from '$lib/translations/translations';
 
-export const load: LayoutLoad = ({ url }) => {
+export const load: LayoutLoad = async ({ url }) => {
+    const { pathname } = url;
+
+    await loadTranslations('de', pathname);
+
     return {
-        pathname: url.pathname,
+        pathname,
     };
 }
